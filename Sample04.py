7e27438beb11f58a48b5ba10236bd8b46bbb4a08
Manabu Kinjo <63534340+m-kinjo3@users.noreply.github.com>
2020-04-12 23:32:19 +0900

    Delete Sample04.py

--- Sample04.py
+++
@@ -1,9 +0,0 @@
-# file handring example
-import os
-
-file=open("/home/mkinjo/Documents/sample.txt",'r')
-print('---------------------------')
-print(file.readline())
-print('---------------------------')
-print(file.read())
-file.close
